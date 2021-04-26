6aed7b441b2634fcf489a61771739ba00a47579d
Tarikan <taras2178@gmail.com>
2021-04-26 14:59:02 +0300

    initial commit

---
+++ AsciiArt.Core/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AsciiArt.Core
+{
+    public class Configuration
+    {
+        public RgbaColor BackgroundColor { get; }
+    }
+
+    public readonly struct RgbaColor
+    {
+        public short R { get; }
+        public short G { get; }
+        public short B { get; }
+        public short A { get; }
+
+        public RgbaColor(short r = 0,
+            short g = 0,
+            short b = 0,
+            short a = 0)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+    }
+}
